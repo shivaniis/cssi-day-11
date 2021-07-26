593cb2d4dac957c10fa8a0615f6abd7572fbdc7a
Shivani V <shivani.venk@gmail.com>
2021-07-26 18:29:23 +0000

    database write successful

--- public/js/writeNote.js
+++ public/js/writeNote.js
@@ -15,5 +15,20 @@ window.onload = () => {
     console.log(createNoteButton)
     createNoteButton.addEventListener("click", () => {
         console.log("clicked")
+        const noteTitle = document.querySelector("#noteTitle").value
+        const noteText = document.querySelector("#noteText").value
+        console.log(noteTitle, noteText)
+
+        firebase.database().ref(`/users/${googleUser.uid}`).push({
+            title: noteTitle,
+            text: noteText
+        }).then(() => {
+            console.log("database write successful")
+            document.querySelector("#noteTitle").value = ""
+            document.querySelector("#noteText").value = ""
+        }).catch(error => {
+            console.log("error writing new note: ", error)
+        })
+
     })
 }
